ae4744011d8f6db6a2f46d637daab8b2ea3dda89
Reinoptland <reinoptland@gmail.com>
2021-04-26 15:50:48 +0200

    Check if one letter is in the word

---
+++ wrongGuesses.js
@@ -0,0 +1,16 @@
+// input
+const word = "javascript";
+const guesses = ["b", "a", "e", "i", "q"];
+
+// desired output output
+// array met wrong guesses:
+// ["b", "e", "q"]
+
+// steps
+// X code schrijven om te checken of 1 letter in het woord zit
+// - naar een functie vertalen
+// - loopen en de functie uitvoeren voor alle elementen
+
+const letter = "b";
+const containsB = word.includes(letter);
+console.log("DOES JAVASCRIPT CONTAIN B:", containsB);
