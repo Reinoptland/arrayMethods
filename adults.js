0e79f3f94973958ec1ccca144773974d2e17176f
Reinoptland <reinoptland@gmail.com>
2021-04-26 16:24:31 +0200

    Ninja version

--- adults.js
+++ adults.js
@@ -4,16 +4,6 @@ const users = [
   { name: "Miriam", age: 28 },
 ];
 
-const adults = users.filter((user) => {
-  console.log("HOW OFTEN DO I GET CALLED?", user, user.age >= 18);
-  // when true is returned -> we keep it
-  // when is false is returned -> we filter it out
-  //   if (user.age >= 18) {
-  //     return user;
-  //   } else {
-  //     return;
-  //   }
-  return user.age >= 18; // true or false
-});
+const adults = users.filter((user) => user.age >= 18);
 
 console.log("OUTPUT:", adults);
