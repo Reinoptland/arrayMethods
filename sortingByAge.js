586bbaee8381957450f80f22eed464bfb033c772
Reinoptland <reinoptland@gmail.com>
2021-04-26 17:00:02 +0200

    Sort by name

--- sortingByAge.js
+++ sortingByAge.js
@@ -1,7 +1,7 @@
 // input
 
 const users = [
-  { name: "Rein", age: 33 },
+  { name: "AAARein", age: 33 },
   { name: "Elias", age: 2 },
   { name: "Miriam", age: 28 },
 ];
@@ -15,5 +15,19 @@ const users = [
 // ];
 
 const sortedByAge = users.sort((userA, userB) => {
-  console.log("INPUT:", userA, userB);
+  console.log("INPUT:", userA.age, userB.age, userA.age - userB.age);
+
+  return userA.age - userB.age;
+  // return value:
+  // positief getal -> userA omlaag
+  // negatief getal -> userA omhoog
+  // 0 -> userA blijft op dezelfde plaats
 });
+
+console.log("OUTPUT:", sortedByAge);
+
+const sortedByName = users.sort(function (userA, userB) {
+  return userA.name.localeCompare(userB.name);
+});
+
+console.log(sortedByName);
