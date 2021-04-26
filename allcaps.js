aacdf9e22b66ba075d0a1830977bc0e67687ed09
Reinoptland <reinoptland@gmail.com>
2021-04-26 15:32:01 +0200

    Add parameter to map

--- allcaps.js
+++ allcaps.js
@@ -4,6 +4,7 @@ const greetings = ["hello", "hi", "how are you"];
 // output: ["HELLO", "HI", "HOW ARE YOU"]
 // transformatie: toUpperCase()
 
-const allCaps = greetings.map(() => {
+const allCaps = greetings.map((greeting) => {
+  console.log("1 greeting", greeting);
   console.log("HOW OFTEN DO I GET CALLED?");
 });
