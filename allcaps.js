aa51feeeaebdb291e4268711b9117254cf52a48e
Reinoptland <reinoptland@gmail.com>
2021-04-26 15:29:28 +0200

    Pass callback to map

--- allcaps.js
+++ allcaps.js
@@ -3,3 +3,7 @@ const greetings = ["hello", "hi", "how are you"];
 
 // output: ["HELLO", "HI", "HOW ARE YOU"]
 // transformatie: toUpperCase()
+
+const allCaps = greetings.map(() => {
+  console.log("HOW OFTEN DO I GET CALLED?");
+});
