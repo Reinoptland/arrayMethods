1d4c2cf4757573228d09da22857ffdf9924d7008
Reinoptland <reinoptland@gmail.com>
2021-04-26 16:19:19 +0200

    Return true or false from callback

--- adults.js
+++ adults.js
@@ -6,4 +6,13 @@ const users = [
 
 const adults = users.filter((user) => {
   console.log("HOW OFTEN DO I GET CALLED?", user, user.age >= 18);
+  // when true is returned -> we keep it
+  // when is false is returned -> we filter it out
+  if (user.age >= 18) {
+    return true;
+  } else {
+    return false;
+  }
 });
+
+console.log("OUTPUT:", adults);
